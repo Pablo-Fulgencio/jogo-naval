234e212451bd51e70e9d33ec511efc026e0cba48
Pablo-Fulgencio <pablofulgencio00@gmail.com>
2026-07-06 17:17:48 -0300

    adicionar comentarios

--- funcoes.h
+++ funcoes.h
@@ -7,6 +7,41 @@
 
 using namespace std;
 
+void imprime_tabuleiro_jogador(char tabuleiro[MAX][MAX], int num_linhas, int num_colunas){
+
+        for (int i = 0; i < num_colunas; i++) {
+            if (i < 10)
+                cout << "\033[102m" << "  " << i;
+            else
+                cout << "\033[102m" << " " << i;
+        }
+        cout << "  " << "\033[0m" << endl;
+
+        for (int i = 0; i < num_linhas; i++) {
+            for (int j = 0; j < num_colunas; j++) {
+
+                if (j == 0) {
+                    if (i > 9)
+                        cout << "\033[102m" << i << "\033[0m";
+                    else
+                        cout << "\033[102m" << i << " " << "\033[0m";
+                }
+
+                if (tabuleiro[i][j] == 'X')
+                    cout << "\033[104;92mX  " << "\033[0m";
+
+                else if (tabuleiro[i][j] == 'O')
+                    cout << "\033[104;91mO  " << "\033[0m";
+
+                else if (tabuleiro[i][j] == '~')
+                    cout << "\033[104;34m~  " << "\033[0m";
+            }
+
+            cout << "  " << "\n";
+        }
+}
+
+//Funcao que recebe as dimensoes do tabuleiro a ser usado durante o jogo e arquivando.
 bool configuracao(){
 
     ofstream conf("config.txt");
@@ -23,21 +58,102 @@ bool configuracao(){
     conf << num_de_embarcacoes;
     conf.close();
 
-//Validacao numero de embarcacoes do arquivo de configuracao:
+    //Validacao numero de embarcacoes do arquivo de configuracao:
+    if(!num_de_linhas || !num_de_colunas || !num_de_embarcacoes) return false;
 
-        if(!num_de_linhas || !num_de_colunas || !num_de_embarcacoes) return false;
-
-        int numero_max_embarcacoes = (num_de_linhas*num_de_colunas)/2;
-        if(num_de_embarcacoes > numero_max_embarcacoes){
-            system("cls");
-            cout << "Erro: quantidade de embarcacoes superior as dimensoes do tabuleiro." << endl;
-            return false;
-        }
-
+    //Numero maximo de embarcacoes possiveis com base no tamanho do tabuleiro.
+    int numero_max_embarcacoes = (num_de_linhas*num_de_colunas)/2;
+    if(num_de_embarcacoes > numero_max_embarcacoes){
+        system("cls");
+        cout << "Erro: quantidade de embarcacoes superior as dimensoes do tabuleiro." << endl;
+        return false;
+    }
     return true;
 }
 
 
+//Funcao menu.
+bool menu(){
+
+        bool  config = false;
+        int linha = 1;
+        menu_visual();
+        while(true){
+            int tecla = _getch();
+                if(tecla == 0 || tecla==224){
+                    tecla = _getch();
+                }
+            switch(tecla){
+                case 72: //Para cima
+                    if(linha == 2){
+                        linha--;
+                        cout << "\033[10;60H";
+                        cout << " ";
+                        cout << "\033[9;60H";
+                        cout << ">";
+                        break;
+                    }
+                    else if(linha == 3){
+                        linha--;
+                        cout << "\033[11;60H";
+                        cout << " ";
+                        cout << "\033[10;60H";
+                        cout << ">";
+                        break;
+                    }
+                    else {
+                        break;
+                    }
+
+                case 80: // Para baixo
+                    if(linha == 1){
+                        linha++;
+                        cout << "\033[9;60H";
+                        cout << " ";
+                        cout << "\033[10;60H";
+                        cout << ">";
+                        break;
+                    }
+                    else if(linha == 2){
+                        linha++;
+                        cout << "\033[10;60H";
+                        cout << " ";
+                        cout << "\033[11;60H";
+                        cout << ">";
+                        break;
+                    }
+                    else {
+                        break;
+                    }
+
+                case 13: // Enter
+                    if(linha == 1){ // jogar
+                        system("cls");
+                        //Verifica se o arquivo de configuracao ja foi preenchido.
+                        if(!config) {
+                            if(!configuracao()) return false;
+                            system("cls");
+                        }
+                        return true;
+                    }
+                    else if(linha == 2){ // configuracao
+                        config = true;
+                        linha = 1;
+                        if (!configuracao()) return false;
+                        system("cls");
+                        menu_visual();
+                        break;
+                    }
+                    else if(linha == 3){ // sair
+                        system("Cls");
+                        return false;
+                    }
+            }
+        }
+}
+
+
+//Funcao que recebe as posicoes das embarcacoes dos respectivos jogadores e armazena em cada arquivo separado.
 bool leitura_jogadores(char nome1[51], char nome2[51]){
 
     ifstream conf("config.txt");
@@ -61,7 +177,6 @@ bool leitura_jogadores(char nome1[51], char nome2[51]){
         cout << "Erro ao abrir arquivo do jogador 2" << endl;
         return false;
     }
-
 
     int linha,coluna;
     char orientacao;
@@ -129,7 +244,7 @@ bool leitura_jogadores(char nome1[51], char nome2[51]){
 }
 
 
-
+//Funcao que valida as condicoes de jogo e retorna se tudo esta ou nao dentro dos limites definidos no arquivo de configuracao.
 bool validacao(char tabuleiro1[MAX][MAX],char tabuleiro2[MAX][MAX]){
 
     ifstream conf("config.txt");
@@ -176,10 +291,10 @@ bool validacao(char tabuleiro1[MAX][MAX],char tabuleiro2[MAX][MAX]){
                 return false;
             }
 
-            if(orientacao == 'V'){
+            if(orientacao == 'V' || orientacao == 'v'){
                 linha += 1;
             }
-            else if(orientacao == 'H'){
+            else if(orientacao == 'H' || orientacao == 'h'){
                 coluna += 1;
             }
             else {
@@ -231,10 +346,10 @@ bool validacao(char tabuleiro1[MAX][MAX],char tabuleiro2[MAX][MAX]){
                 return false;
             }
 
-            if(orientacao == 'V'){
+            if(orientacao == 'V' || orientacao == 'v'){
                 linha += 1;
             }
-            else if(orientacao == 'H'){
+            else if(orientacao == 'H' || orientacao == 'h'){
                 coluna += 1;
             }
             else {
@@ -270,125 +385,12 @@ bool validacao(char tabuleiro1[MAX][MAX],char tabuleiro2[MAX][MAX]){
 }
 
 
+//Verifica se ainda há embarcacoes no tabuleiro.
 bool verifica_numero_celulas(int celulas){
     return celulas;
 }
 
 
-bool menu(){
 
-        bool  config = false;
-        int linha = 1;
-        menu_visual();
-        while(true){
-            int tecla = _getch();
-                if(tecla == 0 || tecla==224){
-                    tecla = _getch();
-                }
-            switch(tecla){
-                case 72: //Para cima
-                    if(linha == 2){
-                        linha--;
-                        cout << "\033[10;60H";
-                        cout << " ";
-                        cout << "\033[9;60H";
-                        cout << ">";
-                        break;
-                    }
-                    else if(linha == 3){
-                        linha--;
-                        cout << "\033[11;60H";
-                        cout << " ";
-                        cout << "\033[10;60H";
-                        cout << ">";
-                        break;
-                    }
-                    else {
-                        break;
-                    }
-
-                case 80: // Para baixo
-                    if(linha == 1){
-                        linha++;
-                        cout << "\033[9;60H";
-                        cout << " ";
-                        cout << "\033[10;60H";
-                        cout << ">";
-                        break;
-                    }
-                    else if(linha == 2){
-                        linha++;
-                        cout << "\033[10;60H";
-                        cout << " ";
-                        cout << "\033[11;60H";
-                        cout << ">";
-                        break;
-                    }
-                    else {
-                        break;
-                    }
-
-                case 13: // Enter
-                    if(linha == 1){ // jogar
-                        system("cls");
-                        if(!config) {
-                            //Verifica se o arquivo de configuracao ja foi preenchido.
-                            if(!configuracao()) return false;
-                            system("cls");
-                        }
-                        return true;
-                    }
-                    else if(linha == 2){ // configuracao
-                        config = true;
-                        linha = 1;
-                        if (!configuracao()) return false;
-                        system("cls");
-                        menu_visual();
-                        break;
-                    }
-                    else if(linha == 3){ // sair
-                        system("Cls");
-                        return false;
-                    }
-            }
-            
-        }
-
-}
-
-void imprime_tabuleiro_jogador(char tabuleiro[MAX][MAX], int num_linhas, int num_colunas){
-
-
-        for (int i = 0; i < num_colunas; i++) {
-            if (i < 10)
-                cout << "\033[102m" << "  " << i;
-            else
-                cout << "\033[102m" << " " << i;
-        }
-        cout << "  " << "\033[0m" << endl;
-
-        for (int i = 0; i < num_linhas; i++) {
-            for (int j = 0; j < num_colunas; j++) {
-
-                if (j == 0) {
-                    if (i > 9)
-                        cout << "\033[102m" << i << "\033[0m";
-                    else
-                        cout << "\033[102m" << i << " " << "\033[0m";
-                }
-
-                if (tabuleiro[i][j] == 'X')
-                    cout << "\033[104;92mX  " << "\033[0m";
-
-                else if (tabuleiro[i][j] == 'O')
-                    cout << "\033[104;91mO  " << "\033[0m";
-
-                else if (tabuleiro[i][j] == '~')
-                    cout << "\033[104;34m~  " << "\033[0m";
-            }
-
-            cout << "  " << "\n";
-        }
-}
 
 
