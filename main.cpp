8643b6524e89e3529221714106c6bea01430be90
Pablo-Fulgencio <pablofulgencio00@gmail.com>
2026-07-06 17:16:58 -0300

    adiciona comentarios

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #define MAX 100
 using namespace std;
 
+//Estrutura com dados do jogo dos jogadores
 struct Jogador{
     char nome[51];
     char tabuleiro[MAX][MAX];
@@ -24,18 +25,18 @@ int main(){
     cout << "Jogador 2:\nDigite seu nome: ";
     cin.getline(jogador2.nome,51);
 
+    //Leitura de numero de linhas,colunas e numero de embarcacoes do arquivo de configuracao:
     ifstream conf("config.txt");
     if(!conf){
-        cout << "Erro" << endl;
+        cout << "Erro ao abrir arquivo de configuracao" << endl;
         return 1;
     }
 
-    //Leitura de numero de linhas,colunas e numero de embarcacoes do arquivo de configuracao:
     int num_linhas,num_colunas,embarcacoes;
     conf >> num_linhas >> num_colunas >> embarcacoes;
     conf.close();
 
-    //inicializacao dos tabuleiros
+    //inicializacao dos tabuleiros.
     for(int i=0;i<num_linhas;i++){
         for(int j=0;j<num_colunas;j++){
             jogador1.tabuleiro[i][j]='~';
@@ -54,13 +55,13 @@ int main(){
 
     ifstream player1("jogador1.txt");
     if(!player1){
-        cout << "Erro" << endl;
+        cout << "Erro ao abrir arquivo de jogador 1" << endl;
         return 1;
     }
 
     ifstream player2("jogador2.txt");
     if(!player2){
-        cout << "Erro" << endl;
+        cout << "Erro ao abrir arquivo de jogador 2" << endl;
         return 1;
     }    
 
@@ -73,32 +74,32 @@ int main(){
         player1 >> coluna >> orientacao;
         jogador1.tabuleiro[linha][coluna]='N';
 
-        if(orientacao == 'V')
+        if(orientacao == 'V' || orientacao == 'v')
             linha+=1;
 
-        else if(orientacao == 'H')
+        else if(orientacao == 'H' || orientacao == 'h')
             coluna+=1;
 
         jogador1.tabuleiro[linha][coluna] = 'N';
     }
+    player1.close();
 
     while(player2 >> linha){
         player2 >> coluna >> orientacao;
         jogador2.tabuleiro[linha][coluna]='N';
 
-        if(orientacao == 'V')
+        if(orientacao == 'V' || orientacao == 'v')
             linha+=1;
 
-        else if(orientacao == 'H')
+        else if(orientacao == 'H' || orientacao == 'h')
             coluna+=1;
 
         jogador2.tabuleiro[linha][coluna] = 'N';
     }
-    player1.close();
     player2.close();
 
 
-    //Jogo jogador 1:
+    //Jogo  do jogador 1:
     int alternador = 0;
     while(true){
 
@@ -106,30 +107,32 @@ int main(){
         if(alternador%2==0){
             alternador += 1;
     
-            cout << "\n\nVez de: " << jogador1.nome<< endl;
-            cout << ".\n.\n." << endl;
-    
-            /* Imprime o tabuleiro de referencia do jogador 1 
-            Tabuleiro de refencia : Tabuleiro que mostra as jogadas realizadas pelo jogador, no tabuleiro adversario
-            sem mostrar as posicoes dos navios nao atingidos */
+            cout << "\n.\n.\n.\nVez de: \n.\n.\n." << jogador1.nome<< endl;
     
             imprime_tabuleiro_jogador(jogador1.tabuleiro_referencia,num_linhas,num_colunas);
             cout << ".\n.\n." << endl;
-
+            /* Imprime o tabuleiro de referencia do jogador. 
+            Tabuleiro de refencia : Tabuleiro que mostra as jogadas realizadas pelo jogador, no tabuleiro adversario
+            sem mostrar as posicoes dos navios nao atingidos */
 
             cout << "Digite a linha e a coluna do disparo: ";
             cin >> linha >> coluna;
+            cin.ignore(1000, '\n');
 
+            //O loop so termina quando a posicao escolhida pelo jogador estiver dentro dos limites do tabuleiro
             while(linha>=num_linhas || coluna>=num_colunas){
                 cout << "\nPonto fora dos limites do tabuleiro" << endl;
                 cout << "Digite novamente as coordenadas do disparo: "; 
                 cin >> linha >> coluna;
+                cin.ignore(1000, '\n');
             }
 
+            //O loop verifica se a posicao escolhida ja nao foi escolhida anteriormente.
             while(jogador1.tabuleiro_referencia[linha][coluna] == 'X' || jogador1.tabuleiro_referencia[linha][coluna] == 'O'){
                 cout << "\nVoce ja realizou um disparo nessa posicao." << endl;
                 cout << "Digite novamente as coordenadas do disparo: "; 
                 cin >> linha >> coluna;    
+                cin.ignore(1000, '\n');
             }
 
             if(jogador2.tabuleiro[linha][coluna] == 'N'){
@@ -139,12 +142,13 @@ int main(){
                 cout << "Tabuleiro atualizado: \n.\n.\n." << endl;
 
                 imprime_tabuleiro_jogador(jogador1.tabuleiro_referencia,num_linhas,num_colunas);
-                cout << "\n.\n.\n." << endl;
+                cout << ".\n.\n." << endl;
             
                 numero_de_celulas_j2--;
                 if(!verifica_numero_celulas(numero_de_celulas_j2)){
                     cout << "Todas as embarcacoes de "<< jogador2.nome << " foram afundadas" << endl;
                     cout << jogador1.nome << " Venceu.\nParabens!" << endl;
+                    cout << ".\n.\n." << endl;
                     return 0;
                 }
             }
@@ -153,8 +157,10 @@ int main(){
                 cout << "Voce errou.\n.\n.\n." << endl;
                 jogador1.tabuleiro_referencia[linha][coluna] = 'O';
 
+                cout << "Tabuleiro atualizado: \n.\n.\n." << endl;
+
                 imprime_tabuleiro_jogador(jogador1.tabuleiro_referencia,num_linhas,num_colunas);
-                cout << "\n.\n.\n." << endl;
+                cout << ".\n.\n." << endl;
             }
             cout << "Pressione qualquer tecla para continuar..." << endl;
             _getch();
@@ -165,32 +171,37 @@ int main(){
         //Jogo Jogador 2:
         else {
             alternador += 1;
-            cout << "\n\nVez de: " << jogador2.nome<< endl;
-            cout << "Tabuleiro: \n.\n.\n." << endl;
 
-                imprime_tabuleiro_jogador(jogador2.tabuleiro_referencia,num_linhas,num_colunas);
-                cout << ".\n.\n." << endl;
+            cout << "\n.\n.\n.\nVez de: \n.\n.\n." << jogador2.nome<< endl;
 
+            imprime_tabuleiro_jogador(jogador2.tabuleiro_referencia,num_linhas,num_colunas);
+            cout << ".\n.\n." << endl;
+            /* Imprime o tabuleiro de referencia do jogador. 
+            Tabuleiro de refencia : Tabuleiro que mostra as jogadas realizadas pelo jogador, no tabuleiro adversario
+            sem mostrar as posicoes dos navios nao atingidos */
 
             cout << "digite a linha e a coluna do disparo: ";
             cin >> linha >> coluna;
-            cin.ignore();
+            cin.ignore(1000, '\n');
 
             while(linha>=num_linhas || coluna>=num_colunas){
                 cout << "\nPonto fora dos limites do tabuleiro" << endl;
                 cout << "Digite novamente as coordenadas do disparo: "; 
                 cin >> linha >> coluna;
+                cin.ignore(1000, '\n');
             }
 
             while(jogador2.tabuleiro_referencia[linha][coluna] == 'X' || jogador2.tabuleiro_referencia[linha][coluna] == 'O'){
                 cout << "\nVoce ja realizou um disparo nessa posicao." << endl;
                 cout << "Digite novamente as coordenadas do disparo: "; 
-                cin >> linha >> coluna;    
+                cin >> linha >> coluna;   
+                cin.ignore(1000, '\n'); 
             }
 
             if(jogador1.tabuleiro[linha][coluna] == 'N'){
                 cout << "Voce acertou uma embarcacao!" << endl;
                 jogador2.tabuleiro_referencia[linha][coluna] = 'X';
+                
                 cout << "Tabuleiro atualizado: \n.\n.\n." << endl;
 
                 imprime_tabuleiro_jogador(jogador2.tabuleiro_referencia,num_linhas,num_colunas);
@@ -207,7 +218,8 @@ int main(){
             else if(jogador1.tabuleiro[linha][coluna] == '~'){
                 cout << "Voce errou." << endl;
                 jogador2.tabuleiro_referencia[linha][coluna] = 'O';
-                cout << "\n.\n.\n." << endl;
+                
+                cout << "Tabuleiro atualizado: \n.\n.\n." << endl;
 
                 imprime_tabuleiro_jogador(jogador2.tabuleiro_referencia,num_linhas,num_colunas);
                 cout << ".\n.\n." << endl;
@@ -218,5 +230,3 @@ int main(){
         }
     }
 }
-
-
