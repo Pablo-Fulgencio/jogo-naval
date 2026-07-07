8780031cd76aef1f975754d20dde617798a8c3d6
Pablo-Fulgencio <pablofulgencio00@gmail.com>
2026-07-06 22:42:35 -0300

    final
    Refactor visual functions to improve clarity and functionality.

--- visual.h
+++ visual.h
@@ -1,25 +1,7 @@
 #pragma once
 #include <iostream>
-#include <ncurses.h>
 
 using namespace std;
-
-void inicializar_ncurses(){
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-    curs_set(0);
-}
-
-void limpar_tela(){
-    clear();
-    refresh();
-}
-
-void finalizar_ncurses(){
-    endwin();
-}
 
 void jogo_naval(){
     cout << "\033[3;54H";
@@ -27,36 +9,39 @@ void jogo_naval(){
     cout << "\n";
 }
 
-void menu_visual(int opcao_selecionada = 1){
-    clear();
-    mvprintw(2, 20, "Bem-Vindo(a) ao Jogo Naval");
-    mvprintw(6, 20, "%s Jogar", opcao_selecionada == 1 ? ">" : " ");
-    mvprintw(8, 20, "%s Configuracoes", opcao_selecionada == 2 ? ">" : " ");
-    mvprintw(10, 20, "%s Sair", opcao_selecionada == 3 ? ">" : " ");
-    refresh();
+void menu_visual(){
+
+    cout << "\033[5;54H";
+    cout << "\033[5;106;31m Bem-Vindo(a) ao Jogo Naval \033[0m" << endl;
+    cout << "\033[9;60H";
+    cout << "> Jogar" << endl;
+    cout << "\033[10;60H";
+    cout << "  Configuracoes" << endl;
+    cout << "\033[11;60H";
+    cout << "  Sair" << endl;
+    cout << "\033[0m";
 }
 
 
 void visual_configuracao(){
     
-    limpar_tela();
+    system("cls");
     jogo_naval();
 
     cout << "\033[32m";
     cout << "\nConsideracoes iniciais:" << endl;
     cout << "Cada partida sera composta por dois jogadores, onde cada um escolhera as posicoes inciais de suas embarcacoes." << endl;
-    cout << "Durante a partida, cada jogador escolhera, quando sua vez, uma posicao do tabuleiro para atacar" << endl;
-    cout << "As posicoes de ataque sao organizadas em x e y, onde o primeiro digito refere-se a posicao x e o segundo a posicao y do tabuleiro." << endl;
-    cout << "Durante os ataques, nao eh necessario escolher uma orientacao (V ou H)" << endl;
+    cout << "Durante a partida, cada jogador escolhera, quando sua vez, uma posicao do tabuleiro inimigo para atacar." << endl;
+    cout << "As posicoes de ataque sao organizadas em x e y, onde o primeiro digito refere-se a linha e o segundo a coluna do tabuleiro." << endl;
     cout << "Nao esqueca que a posicao inicial do tabuleiro comeca em (0,0)!!" << endl;
-    cout << "Para comecar, defina primeiro as dimensoes iniciais do tabuleiro: Numero de linhas e Colunas, respectivamente: Logo em seguida, defina o numero de embarcacoes da partida" << endl; 
+    cout << "Para comecar, defina primeiro as dimensoes iniciais do tabuleiro: Numero de linhas e Colunas e o numero de embarcacoes da partida" << endl; 
     cout << "\033[0m";
 }
 
 
-void visual_leitura_jogador(char nome[51]){
+void visual_leitura_jogador(char nome[]){
 
-    limpar_tela();
+    system("cls");
     jogo_naval();
 
     cout << nome << " Digite as linhas e as colunas das suas embarcacoes" << endl;
